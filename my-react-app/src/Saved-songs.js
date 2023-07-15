fe0e5ac6080958a5e88210b1c5c5896c59e8bbb1
MertcanYildirim078 <99068314@mydavinci.nl>
2023-07-15 15:37:31 +0200

    saved songs filter added and connection, not 100%

--- my-react-app/src/Saved-songs.js
+++ my-react-app/src/Saved-songs.js
@@ -2,20 +2,60 @@ import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route, Link, useNavigate } from 'react-router-dom';
 
 function Savedsongs() {
-    return (
-      <div className='body-Savedsongs'>
-        <main className="mainContainerSavedsongs">
-          <nav className='navBar'>
-            <ul className='navBarContainer'>
-              <li style={{ position: 'absolute', left: '0' }}><a className="logOutButton" href='/'>Log out</a></li>
-              <li><a href='/dashboard'>Songs</a></li>
-              <li><a href='/savedsongs'>Saved songs</a></li>
-            </ul>
-          </nav>
-        </main>
-      </div>
-    );
-  }
-  
-  export default Savedsongs;
-   
+  const [songs, setSongs] = useState([]);
+  const [filteredSongs, setFilteredSongs] = useState([]);
+  const [genreFilter, setGenreFilter] = useState('');
+
+  useEffect(() => {
+    fetch('/api/saved_songs_list')
+      .then(response => response.json())
+      .then(data => {
+        setSongs(data);
+        setFilteredSongs(data);
+      })
+      .catch(error => console.error('Error retrieving data:', error));
+  }, []);
+
+  const handleGenreFilterChange = (event) => {
+    const { value } = event.target;
+    setGenreFilter(value);
+
+    // Filter bij input
+    const filtered = songs.filter(song => song.genre.toLowerCase().includes(value.toLowerCase()));
+    setFilteredSongs(filtered);
+  };
+
+  return (
+    <div className='body-Savedsongs'>
+      <main className="mainContainerSavedsongs">
+        <nav className='navBar'>
+          <ul className='navBarContainer'>
+            <li style={{ position: 'absolute', left: '0' }}><a href='/'>Log out</a></li>
+            <li><a href='/dashboard'>Songs</a></li>
+            <li><a href='/savedsongs'>Saved songs</a></li>
+          </ul>
+        </nav>
+
+        <h1>Saved songs</h1>
+        <input type="text" placeholder="Filter by genre" value={genreFilter} onChange={handleGenreFilterChange} />
+
+        <ul className='songsList'>
+          {Array.isArray(filteredSongs) ? (
+            filteredSongs.map(song => (
+              <div key={song.id}>
+                <p>
+                  {song.name} - {song.author} - {song.genre}
+                </p>
+              </div>
+            ))
+          ) : (
+            <p>No songs found.</p>
+          )}
+      </ul>
+
+      </main>
+    </div>
+  );
+}
+
+export default Savedsongs;
