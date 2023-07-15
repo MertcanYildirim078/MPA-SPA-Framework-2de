bbd31fdf84e4157fba5ccd242032a2bc04b9f49e
MertcanYildirim078 <99068314@mydavinci.nl>
2023-07-15 14:48:41 +0200

    genre filter added

--- my-react-app/src/Dashboard.js
+++ my-react-app/src/Dashboard.js
@@ -2,43 +2,56 @@ import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route, Link, useNavigate } from 'react-router-dom';
 
 function Dashboard() {
-
   const [songs, setSongs] = useState([]);
+  const [filteredSongs, setFilteredSongs] = useState([]);
+  const [genreFilter, setGenreFilter] = useState('');
 
   useEffect(() => {
     fetch('/api/songs')
       .then(response => response.json())
-      .then(data => setSongs(data))
+      .then(data => {
+        setSongs(data);
+        setFilteredSongs(data);
+      })
       .catch(error => console.error('Error retrieving data:', error));
   }, []);
 
-    return (
-      <div className='body-dashboard'>
-        <main className="mainContainerDashboard">
+  const handleGenreFilterChange = (event) => {
+    const { value } = event.target;
+    setGenreFilter(value);
 
-          <nav className='navBar'>
-            <ul className='navBarContainer'>
-              <li style={{ position: 'absolute', left: '0' }}><a href='/'>Log out</a></li>
-              <li><a href='/dashboard'>Songs</a></li>
-              <li><a href='/savedsongs'>Saved songs</a></li>
-            </ul>
-          </nav>
+    // Filter bij input
+    const filtered = songs.filter(song => song.genre.toLowerCase().includes(value.toLowerCase()));
+    setFilteredSongs(filtered);
+  };
 
-          <h1>Songs List</h1>
+  return (
+    <div className='body-dashboard'>
+      <main className="mainContainerDashboard">
+        <nav className='navBar'>
+          <ul className='navBarContainer'>
+            <li style={{ position: 'absolute', left: '0' }}><a href='/'>Log out</a></li>
+            <li><a href='/dashboard'>Songs</a></li>
+            <li><a href='/savedsongs'>Saved songs</a></li>
+          </ul>
+        </nav>
+
+        <h1>Songs List</h1>
+        <input type="text" placeholder="Filter by genre" value={genreFilter} onChange={handleGenreFilterChange} />
+
         <ul className='songsList'>
-          {songs.map(song => (
+          {filteredSongs.map(song => (
             <div key={song.id}>
               <p>
-                {song.name} - {song.author}
+                {song.name} - {song.author} - {song.genre}
               </p>
             </div>
           ))}
         </ul> 
 
-        </main>
-      </div>
-    );
-  }
-  
-  export default Dashboard;
-   
+      </main>
+    </div>
+  );
+}
+
+export default Dashboard;
